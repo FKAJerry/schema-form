366dfcc6fd0e6a571baf27fb6b383b97514cbdb3
hermaproditus <zxx1991@live.com>
2019-08-28 14:27:58 +0800

    [fix] fix integer value

--- src/core/lib/formly-dao-style/fields/integer.vue
+++ src/core/lib/formly-dao-style/fields/integer.vue
@@ -1,60 +1,58 @@
 <template>
-  <dao-setting-section>
-    <dao-setting-item>
-      <template slot="label" v-if="to.label">
-        {{ to.label }}
-      </template>
-      <template slot="content">
-        <dao-input
-          ref="integer"
-          type="text"
-          :class="to.classes"
-          :id="to.id ? to.id : null"
-          :placeholder="getAtts(to.atts, 'placeholder')"
-          :status="status"
-          :message="message"
-          :disabled="to.disabled"
-          v-model="model[field.key]"
-          icon-inside
-          @blur="onBlur"
-          @focus="onFocus"
-          @click="onClick"
-          @change="onChange"
-          @keyup="onKeyup"
-          @keydown="onKeydown"
-          :append-to-body="false"
-          v-formly-atts="to.atts"
-          v-formly-input-type="to.inputType">
-          <span
-            v-if="to.unit"
-            slot="append">
-            {{ to.unit }}
-          </span>
-        </dao-input>
-      </template>
-      <p slot="content-helper" v-if="to.description">
-        {{ to.description }}
-      </p>
-    </dao-setting-item>
-  </dao-setting-section>
+  <field-item :to="to">
+    {{ model[field.key] }}
+    <dao-input
+      ref="integer"
+      type="number"
+      :class="to.classes"
+      :id="to.id ? to.id : null"
+      :placeholder="getAtts(to.atts, 'placeholder')"
+      :status="status"
+      :message="message"
+      :disabled="to.disabled"
+      v-model.number="model[field.key]"
+      icon-inside
+      @blur="onBlur"
+      @focus="onFocus"
+      @click="onClick"
+      @change="onChange"
+      @keyup="onKeyup"
+      @keydown="onKeydown"
+      :append-to-body="false"
+      v-formly-atts="to.atts"
+      v-formly-input-type="to.inputType">
+      <span
+        v-if="to.unit"
+        slot="append">
+        {{ to.unit }}
+      </span>
+    </dao-input>
+  </field-item>
 </template>
 
 <script>
 import baseField from './baseField';
+import FieldItem from '../components/field-item';
 
 export default {
+  components: {
+    FieldItem,
+  },
+
   mixins: [baseField],
+
   data() {
     return {
       status: '',
       message: '',
     };
   },
+
   methods: {
     onChange(e) {
       if (e === '') return;
 
-      if (!this.checkIsValidNumber(e.trim())) {
+      if (!this.checkIsValidNumber(e)) {
         this.status = 'error';
         this.message = '请输入合法的字符';
       } else {
@@ -80,7 +78,7 @@ export default {
 
       const { min = -Infinity, max = Infinity } = this.getIntegerRange();
       if (integer >= min && integer <= max) {
-        this.setModel(`${integer}`);
+        this.setModel(integer);
         return true;
       }
 
